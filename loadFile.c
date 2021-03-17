07bccf1d382bfb76d9f1743e0728337d73fd7312
kharismajd <kharismajagaddhita170501@gmail.com>
2021-03-17 19:16:18 +0700

    fix loadFile

--- loadFile.c
+++ loadFile.c
@@ -1,132 +1,139 @@
-//loadFile.c
-//Michael Black, 2007
-//Modified by Fred Zhang, All modifications are commented with Changed.
-//		Those changes are used help implement directories.
-//
-//Loads a file into the file system
-//This should be compiled with gcc and run outside of the OS
-
 #include <stdio.h>
 
-main(int argc, char* argv[])
-{
-	int i;
-
-	if (argc<2)
-	{
-		printf("Specify file name to load\n");
-		return;
-	}
-
-	//open the source file
-	FILE* loadFil;
-	loadFil=fopen(argv[1],"r");
-	if (loadFil==0)
-	{
-		printf("File not found\n");
-		return;
-	}
-
-	//open the floppy image
-	FILE* floppy;
-	floppy=fopen("system.img","r+");
-	if (floppy==0)
-	{
-		printf("system.img not found\n");
-		return;
-	}
-
-	//load the disk map
+void main(int argc, char* argv[]) {
+  	FILE* file_to_load;
+	FILE* system;
+	  
 	char map[512];
-	fseek(floppy,512,SEEK_SET);
-	for(i=0; i<512; i++)
-		map[i]=fgetc(floppy);
+	char files[1024];
+	char sectors[512];
 
-	//load the directory
-	char dir[512];
-	fseek(floppy,512*2,SEEK_SET);
-	for (i=0; i<512; i++)
-		dir[i]=fgetc(floppy);
+	char c;
 
-	//find a free entry in the directory
-	// Changed: Start to search from the second slot, 
-	// since the first one stored the information about this directory
-	for (i=0x20; i<512; i=i+0x20) 
-		if (dir[i]==0)
-			break;
-	if (i==512)
-	{
-		printf("Not enough room in directory\n");
+	int i; 
+	int fileIndex;
+	int emptyFileIndex;
+	int sectorIndex;
+	int emptySectorIndex;
+	int mapIndex;
+	int emptyMapIndex;
+
+	if (argc < 2) {
+		printf("Ketik nama file yang ingin di-load\n");
 		return;
 	}
-	int dirindex=i;
-	//fill the name field with 00s first
-	for (i=0; i<6; i++)
-		dir[dirindex+i]=0x00;
-	//copy the name over
-	for (i=0; i<6; i++)
-	{
-		if(argv[1][i]==0)
-			break;
-		dir[dirindex+i]=argv[1][i];
+
+	file_to_load = fopen(argv[1], "r");
+	if (file_to_load == 0) {
+		printf("File tidak ditemukan\n");
+		return;
+	}
+	system = fopen("system.img", "r+");
+	if (system == 0) {
+		printf("system.img tidak ditemukan\n");
+		return;
 	}
 
-	dirindex=dirindex+6;
+	fseek(system, 512 * 0x100, SEEK_SET);
+	for (i = 0; i < 512; i++) { 
+		map[i] = fgetc(system);
+	}
+	fseek(system, 512 * 0x101, SEEK_SET);
+	for (i = 0; i < 1024; i++) {
+		files[i] = fgetc(system);
+	}
+	fseek(system, 512 * 0x103, SEEK_SET);
+	for (i = 0; i < 512; i++) {
+		sectors[i] = fgetc(system);
+	}
 
-	//find free sectors and add them to the file
-	int sectcount=1;
-	while(!feof(loadFil))
-	{
-		if (sectcount==25) // Changed: the last bit is used as info bit
-		{
-			printf("Not enough space in directory entry for file\n");
+	emptyFileIndex = -1;
+	for (fileIndex = 0; fileIndex < 64; fileIndex++) {
+		if (files[fileIndex * 16] == 0 && files[fileIndex * 16 + 1] == 0 && files[fileIndex * 16 + 2] == 0) {
+			emptyFileIndex = fileIndex;
+			break;
+		} 
+	}
+	if (emptyFileIndex == -1) {
+		printf("Tidak cukup entri di files\n");
+		return;
+	}
+
+	emptySectorIndex = -1;
+	for (sectorIndex = 0; sectorIndex < 32; sectorIndex++)
+		if (sectors[sectorIndex * 16] == 0) {
+			emptySectorIndex = sectorIndex;
+			break;
+		}
+	if (emptySectorIndex == -1) {
+		printf("Tidak cukup sektor kosong\n");
+		return;
+	}
+	
+	// clear
+	for (i = 0; i < 14; i++) {
+		files[fileIndex * 16 + 2 + i] = 0x0;
+	}
+	files[fileIndex * 16] = 0xFF;
+	files[fileIndex * 16 + 1] = emptySectorIndex;
+	for (i = 0; i < 14; i++) {
+		files[fileIndex * 16 + 2 + i] = argv[2][i];
+	}
+
+	int sectcount = 0;
+	while (!feof(file_to_load)) {
+		if (sectcount == 16) {
+			printf("Ukuran file terlalu besar\n");
 			return;
 		}
 
-		//find a free map entry
-		for (i=0; i<256; i++)
-			if (map[i]==0)
-				break;
-		if (i==256)
-		{
-			printf("Not enough room for file\n");
-			return;
-		}
-
-		//mark the map entry as taken
-		map[i]=0xFF;
-
-		//mark the sector in the directory entry
-		dir[dirindex]=i;
-		dirindex++;
-		sectcount++;
-
-		//move to the sector and write to it
-		fseek(floppy,i*512,SEEK_SET);
-		for (i=0; i<512; i++)
-		{
-			if (feof(loadFil))
-			{
-				fputc(0x0, floppy);
+		emptyMapIndex = -1;
+		for (mapIndex = 0; mapIndex < 512; mapIndex++) {
+			if (map[mapIndex] == 0) {
+				emptyMapIndex = mapIndex;
 				break;
 			}
-			else
-			{
-				char c = fgetc(loadFil);
-				fputc(c, floppy);
-			}	
 		}
+		if (emptyMapIndex == -1) {
+			printf("Tidak cukup sektor kosong\n");
+			return;
+		}
+
+		map[emptyMapIndex] = 0xFF;
+		sectors[emptySectorIndex] = emptyMapIndex;
+		
+		printf("File %s diletakan pada sektor %d\n", argv[2], emptyMapIndex);
+
+		fseek(system, emptyMapIndex * 512, SEEK_SET);
+		for (i = 0; i < 512; i++) {
+			if (feof(file_to_load)) {
+				fputc(0x0, system);
+				break;
+			} else {
+				c = fgetc(file_to_load);
+				fputc(c, system);
+			}
+		}
+
+		emptySectorIndex++;
+		sectcount++;
 	}
 
-	//write the map and directory back to the floppy image
-        fseek(floppy,512,SEEK_SET);
-        for(i=0; i<512; i++)
-		fputc(map[i],floppy);
-        
-        fseek(floppy,512*2,SEEK_SET);
-        for (i=0; i<512; i++)
-		fputc(dir[i],floppy);
+	fseek(system, 512 * 0x100, SEEK_SET);
+	for (i = 0; i < 512; i++) {
+		fputc(map[i], system);
+	}
+	fseek(system, 512 * 0x101, SEEK_SET);
+	for (i = 0; i < 1024; i++) {
+		fputc(files[i], system);
+	}
+	fseek(system, 512 * 0x103, SEEK_SET);
+	for (i = 0; i < 512; i++){
+		fputc(sectors[i], system);
+	}
 
-	fclose(floppy);
-	fclose(loadFil);
+	printf("Load file berhasil");
+
+	fclose(system);
+	fclose(file_to_load);
 }
